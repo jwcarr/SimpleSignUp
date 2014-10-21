175988db3bab7c641caa96b7001f8c4bd5317afc
Jon Carr <jwcarr@users.noreply.github.com>
2014-10-21 17:46:53 +0100

    Check for exclusions
    Before the calendar page is shown, the PHP code now checks any
    exclusion criteria that might apply and shows the not_eligible page if
    necessary.

--- php/calendar.php
+++ php/calendar.php
@@ -4,4 +4,24 @@ include("file.php");
 
 $experiment = new Experiment($_REQUEST['exp']);
 
+$excluded_email_addresses = $experiment->getExclusionEmails();
+
+foreach ($experiment->getExclusions() as $exclusion) {
+  $alt_experiment = new Experiment($exclusion);
+  $excluded_email_addresses = array_merge($excluded_email_addresses, $alt_experiment->getExclusionEmails());
+}
+
+$target_email = $_REQUEST['email'];
+
+if (in_array($target_email, $excluded_email_addresses)) {
+  $page = 'not_eligible';
+  $user = new User($experiment->owner);
+}
+
+else {
+  if ($experiment->getPerSlot() > 1) {
+    $additional_message = "<strong>This experiment requires {$experiment->getPerSlot()} participants per timeslot.</strong> You will not be guaranteed a place on the experiment until {$experiment->getPerSlot()} people have signed up for your slot. Where possible, please choose a slot that someone else has already signed up for (highlighted in orange). You’ll receive an email to confirm when your slot has been filled.";
+  }
+}
+
 ?>
