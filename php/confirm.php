e7dec0ba59b366b2116eb318e355040345cc2109
Jon Carr <jwcarr@users.noreply.github.com>
2014-11-03 21:32:09 +0000

    Send email on successful sign up
    Once the data has been written out and there are no issues, send the
    participant a confirmation email. Also, if this is a multi-person
    experiment and the slot is now full, send emails to the other people in
    the slot to let them know that the slot has been filled.

--- php/confirm.php
+++ php/confirm.php
@@ -33,11 +33,33 @@ else {
       }
     }
 
+    // Get a list of the participants who have currently signed up for this slot
+    $current_subjects = $experiment->getSlot($_REQUEST['timeslot']);
+
     // Double check that the timeslot is still free
-    if (count($experiment->getSlot($_REQUEST['timeslot'])) < $experiment->getPerSlot()) {
+    if (count($current_subjects) < $experiment->getPerSlot()) {
+      // Set the time slot with the participant's details
       $experiment->setSlot($_REQUEST['timeslot'], $_REQUEST['name'], $_REQUEST['email'], $_REQUEST['phone']);
+      // Add the participant's email to the list of excluded email addresses
       $experiment->addExclusionEmails(array($_REQUEST['email']));
-      if ($experiment->saveExperimentData() == False) {
+      // If the new data is successfully written out...
+      if ($experiment->saveExperimentData() == True) {
+        // If this is a multi-person experiment AND the slot is now full...
+        if ($experiment->getPerSlot() > 1 AND $experiment->getPerSlot() == count($current_subjects)+1) {
+          // For each participant who already signed up...
+          foreach ($current_subjects as $subject) {
+            // Send an email to say that the experiment will go ahead as planned
+            $experiment->sendEmail($subject[1], $user->getName(), $user->getEmail(), 'email_full', array('NAME'=>$subject[0], 'DATE'=>$slot_date, 'TIME'=>$slot_time));
+          }
+          // Send an email to this participant to confirm the appointment
+          $experiment->sendEmail($_REQUEST['email'], $user->getName(), $user->getEmail(), 'email_conf_full', array('NAME'=>$_REQUEST['name'], 'DATE'=>$slot_date, 'TIME'=>$slot_time));
+        }
+        else {
+          // Send an email to this participant to confirm the booking
+          $experiment->sendEmail($_REQUEST['email'], $user->getName(), $user->getEmail(), 'email_conf', array('NAME'=>$_REQUEST['name'], 'DATE'=>$slot_date, 'TIME'=>$slot_time));
+        }
+      }
+      else {
         $page = 'error';
         $error = '100';
       }
@@ -47,8 +69,6 @@ else {
       $page = 'calendar';
       $warning_message = True;
     }
-
-    //$a = mail($_REQUEST['email'], $experiment->getName(), "Dear Jon", "From: {$user->getName()} <{$user->getEmail()}>");
   }
 }
 
