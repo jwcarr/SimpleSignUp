d82d4587e2051143d630bde16262285153716f61
Jon Carr <jwcarr@users.noreply.github.com>
2014-12-01 11:50:34 +0000

    Added delete subject page

---
+++ admin/php/delete_subject.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once("../php/file.php");
+
+$user = new User($username);
+
+if (isset($_REQUEST['confirm'])) {
+
+  $experiment = new Experiment($_REQUEST['exp'], True);
+  $subject = $experiment->getSubject($_REQUEST['date'], $_REQUEST['time'], $_REQUEST['subject']);
+  $experiment->deleteSubject($_REQUEST['date'], $_REQUEST['time'], $_REQUEST['subject']);
+
+  if ($experiment->saveExperimentData()) {
+    $notification = '"'. $subject[0] .'" has been removed from this experiment.';
+  }
+  else {
+    $notification = 'There was an error removing "'. $subject[0] .'" from this experiment.';
+  }
+
+  $notification = '<div id="notification"><p>' . $notification . '</p></div>';
+
+  include('php/view.php');
+  $page = 'view';
+
+}
+else {
+  $experiment = new Experiment($_REQUEST['exp'], False);
+  $subject = $experiment->getSubject($_REQUEST['date'], $_REQUEST['time'], $_REQUEST['subject']);
+}
+
+?>
