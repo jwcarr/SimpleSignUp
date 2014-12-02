f2543f20b6e5478860442a689386b1d58549b627
Jon Carr <jwcarr@users.noreply.github.com>
2014-12-02 14:02:42 +0000

    Added generateMenu() function
    This generates the right-hand menu, de-hyperlinking the current page

--- admin/php/globals.php
+++ admin/php/globals.php
@@ -12,4 +12,19 @@ function cleanInputVariables() {
   }
 }
 
+function generateMenu($name, $current_page) {
+  $menu_items = array('main'=>'My experiments', 'new'=>'New experiment', 'change_details'=>'Change my details', 'change_password'=>'Change my password');
+  $menu = '<p><strong>'. $name .'</strong></p><ul>';
+  foreach ($menu_items as $page=>$description) {
+    if ($page == $current_page) {
+      $menu .= '<li>'. $description .'</li>';
+    }
+    else {
+      $menu .= '<li><a href="index.php?page='. $page .'">'. $description .'</a></li>';
+    }
+  }
+  $menu .= '<li><a href="index.php?page=logout">Logout</a></li></ul>';
+  return $menu;
+}
+
 ?>
