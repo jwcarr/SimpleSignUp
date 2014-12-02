9fe974adfbd16af4aa1c50870ce77d14921b5017
Jon Carr <jwcarr@users.noreply.github.com>
2014-12-02 22:41:21 +0000

    Removed cleanInputVariables() for admin
    input variables will be cleaned on a case-by-case basis within the
    admin area

--- php/globals.php
+++ php/globals.php
@@ -6,10 +6,4 @@ $data_path = '../../server_data/ssu/';
 // Set timezone for timestamps (default = UTC)
 date_default_timezone_set('UTC');
 
-function cleanInputVariables() {
-  foreach ($_REQUEST as $key=>$value) {
-    $_REQUEST[$key] = str_replace(array('"', ',' , '~', ';', '{', '}', '[', ']', '=', '&'), '', str_replace("'", '’', trim($value)));
-  }
-}
-
 ?>
