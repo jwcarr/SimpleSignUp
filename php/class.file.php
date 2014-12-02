6d2ee67178a52578295d583a500e102ddfe18d13
Jon Carr <jwcarr@users.noreply.github.com>
2014-12-02 07:48:36 +0000

    Added separate file for File class

---
+++ php/class.file.php
@@ -0,0 +1,88 @@
+<?php
+
+class File {
+
+  private $filename = '';
+  private $write_access = False;
+  public $data = '';
+
+  public function __construct($filename, $write_access=False) {
+    $this->filename = $filename;
+    $this->write_access = $write_access;
+    $this->data = $this->openFile();
+  }
+
+  public function __destruct() {
+    if ($this->write_access) {
+      flock($this->file, LOCK_UN);
+      fclose($this->file);
+    }
+  }
+
+  private function openFile() {
+    if ($this->write_access) {
+      return $this->openFileWithWriteAccess();
+    }
+    else {
+      return $this->openFileWithoutWriteAccess();
+    }
+  }
+
+  private function openFileWithoutWriteAccess() {
+    if (file_exists($this->filename)) {
+      $this->file = fopen($this->filename, 'r');
+      if ($this->file != False) {
+        if (flock($this->file, LOCK_SH)) {
+          $data = fread($this->file, filesize($this->filename));
+          flock($this->file, LOCK_UN);
+          fclose($this->file);
+          return $data;
+        }
+        fclose($this->file);
+      }
+    }
+    return False;
+  }
+
+  private function openFileWithWriteAccess() {
+    if (file_exists($this->filename)) {
+      for ($i=0; $i<10; $i++) {
+        if (is_writable($this->filename)) {
+          $this->file = fopen($this->filename, 'a+');
+          if ($this->file != False) {
+            if (flock($this->file, LOCK_EX)) {
+              $data = fread($this->file, filesize($this->filename));
+              return $data;
+            }
+            fclose($this->file);
+          }
+        }
+        sleep(1);
+      }
+      $this->write_access = False;
+      return $this->openFileWithoutWriteAccess();
+    }
+    $this->write_access = False;
+    return False;
+  }
+
+  public function overwrite() {
+    if ($this->write_access) {
+      if (ftruncate($this->file, 0)) {
+        if (fwrite($this->file, $this->data)) {
+          flock($this->file, LOCK_UN);
+          fclose($this->file);
+          $this->write_access = False;
+          return True;
+        }
+      }
+      flock($this->file, LOCK_UN);
+      fclose($this->file);
+    }
+    $this->write_access = False;
+    return False;
+  }
+
+}
+
+?>
