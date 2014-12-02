65c9d38f30af1280c097ca32dc546fdb53fb8faa
Jon Carr <jwcarr@users.noreply.github.com>
2014-12-02 22:43:14 +0000

    PHP script for creating new experiment

--- admin/php/new.php
+++ admin/php/new.php
@@ -1,4 +1,50 @@
 <?php
 
+if (isset($_REQUEST['confirm'])) {
+
+  unset($user);
+  $user = new User($identity[0], True);
+  $user->addExperiment($_REQUEST['exp']);
+
+  $experiments_file = new File($data_path .'experiments', True);
+  $experiments_file->data = $experiments_file->data . "\n" . $_REQUEST['exp'] . ' = {' . $identity[0] . '}';
+
+  $f = fopen($data_path . 'user_data/'. $identity[0] . '/' . $_REQUEST['exp'], 'w');
+  fclose($f);
+
+  $experiment = new Experiment($_REQUEST['exp'], True, $identity[0]);
+  $experiment->setName($_REQUEST['name']);
+  $experiment->setStatus('closed');
+  $experiment->setDescription($_REQUEST['description']);
+  $experiment->setLocation($_REQUEST['location']);
+  //requirements
+  //exclusions
+  $experiment->setPerSlot($_REQUEST['per_slot']);
+  //calendar
+  //email_conf
+  //email_full
+  //email_conf_full
+
+  if ($experiment->saveExperimentData()) {
+    if ($experiments_file->overwrite()) {
+      if ($user->saveUserDetails()) {
+        $page = 'main';
+        $notification = 'Your new experiment has been created.';
+      }
+      else {
+        $notification = '<strong>Error:</strong> failure to add experiment to this user.';
+      }
+    }
+    else {
+      $notification = '<strong>Error:</strong> failure to add experiment to SimpleSignUp.';
+    }
+  }
+  else {
+    $notification = '<strong>Error:</strong> failure to create experiment.';
+  }
+
+  $notification = '<div id="notification"><p>' . $notification . '</p></div>';
+
+}
 
 ?>
