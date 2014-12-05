97b2f54967922b6758777e442971c67cde35bdac
Jon Carr <jwcarr@users.noreply.github.com>
2014-12-05 21:30:49 +0000

    Generate and show a bar chart showing progress
    Shows a bar chart at the top of the view page, showing various stats
    such as how many participants have been completed and how many slots
    are still free.

--- admin/php/view.php
+++ admin/php/view.php
@@ -9,76 +9,109 @@ if (isset($experiment) == False) {
 $unix_today = strtotime(date('Y-m-d'));
 $unix_tomorrow = $unix_today + 86400;
 
-foreach ($experiment->getCalendar() as $date=>$slots) {
-  $unix_date = strtotime($date);
+if ($experiment->getCalendar() != None) {
 
-  if ($unix_date < $unix_today) {
-    if ($time_point != 'past') {
-      $schedule .= '<div id="view-title-past" class="view-title"><h3>► Completed</h3></div><div id="view-past" class="view-table"><table style="width: 100%; border-spacing: 10px;">';
-      $time_point = 'past';
-    }
-  }
-  elseif ($unix_date == $unix_today) {
-    if ($time_point == 'past') {
-      $schedule .= '</table></div>';
-    }
-    if ($time_point != 'present') {
-      $schedule .= '<div id="view-title-present" class="view-title"><h3>► Today</h3></div><div id="view-present" class="view-table"><table style="width: 100%; border-spacing: 10px;">';
-      $time_point = 'present';
-    }
-  }
-  elseif ($unix_date > $unix_today) {
-    if ($time_point == 'past' OR $time_point == 'present') {
-      $schedule .= '</table></div>';
-    }
-    if ($time_point != 'future') {
-      $schedule .= '<div id="view-title-future" class="view-title"><h3>► Upcoming</h3></div><div id="view-future" class="view-table"><table style="width: 100%; border-spacing: 10px;">';
-      $time_point = 'future';
-    }
-  }
+  $free_slots = 0;
+  $past_slots = 0;
+  $present_slots = 0;
+  $future_slots = 0;
 
-  $schedule .= '<tr><td colspan="5"><strong>'. date('l, jS F Y', $unix_date) .'</strong></td></tr>';
+  foreach ($experiment->getCalendar() as $date=>$slots) {
+    $unix_date = strtotime($date);
 
-  foreach ($slots as $time=>$slot) {
-    for ($i=0; $i<$experiment->getPerSlot(); $i++) {
-      if ($i == 0) { $show_time = $time; } else { $show_time = ''; }
-      if ($slot == None) {
-        $schedule .= '<tr>
-        <td width="10%">' . $show_time . '</td>
-        <td width="25%">-</td>
-        <td width="35%">-</td>
-        <td width="20%">-</td>
-        <td width="10%"></td>
-        </tr>';
+    if ($unix_date < $unix_today) {
+      if ($time_point != 'past') {
+        $schedule .= '<div id="view-title-past" class="view-title"><h3>► Completed</h3></div><div id="view-past" class="view-table"><table style="width: 100%; border-spacing: 10px;">';
+        $time_point = 'past';
       }
-      else {
-        if (isset($slot[$i])) {
-          $subject = $slot[$i];
-          $schedule .= '<tr>
-          <td width="10%">' . $show_time . '</td>
-          <td width="25%">' . $subject[0] . '</td>
-          <td width="35%">' . $subject[1] .'</td>
-          <td width="20%">' . $subject[2] . '</td>
-          <td width="10%"><a href="index.php?page=edit_subject&exp='. $experiment->id .'&date='. $date .'&time='. $time .'&subject='. $i .'">✎</a>&nbsp;<a href="index.php?page=delete_subject&exp='. $experiment->id .'&date='. $date .'&time='. $time .'&subject='. $i .'">✘</a></td>
-          </tr>';
+    }
+    elseif ($unix_date == $unix_today) {
+      if ($time_point == 'past') {
+        $schedule .= '</table></div>';
+      }
+      if ($time_point != 'present') {
+        $schedule .= '<div id="view-title-present" class="view-title"><h3>► Today</h3></div><div id="view-present" class="view-table"><table style="width: 100%; border-spacing: 10px;">';
+        $time_point = 'present';
+      }
+    }
+    elseif ($unix_date > $unix_today) {
+      if ($time_point == 'past' OR $time_point == 'present') {
+        $schedule .= '</table></div>';
+      }
+      if ($time_point != 'future') {
+        $schedule .= '<div id="view-title-future" class="view-title"><h3>► Upcoming</h3></div><div id="view-future" class="view-table"><table style="width: 100%; border-spacing: 10px;">';
+        $time_point = 'future';
+      }
+    }
+
+    $schedule .= '<tr><td colspan="5"><strong>'. date('l, jS F Y', $unix_date) .'</strong></td></tr>';
+
+    foreach ($slots as $time=>$slot) {
+      for ($i=0; $i<$experiment->getPerSlot(); $i++) {
+        if ($i == 0) {
+          $show_time = $time;
+          $show_plus = '<a href="index.php?page=add_subject&exp='. $experiment->id .'&date='. $date .'&time='. $time .'"><img src="images/add.png" width="16" height="16" style="vertical-align: bottom" /></a>';
         }
         else {
+          $show_time = '';
+          $show_plus = '';
+        }
+        if ($slot == None) {
           $schedule .= '<tr>
           <td width="10%">' . $show_time . '</td>
           <td width="25%">-</td>
           <td width="35%">-</td>
           <td width="20%">-</td>
-          <td width="10%"></td>
+          <td width="10%">' . $show_plus . '</td>
           </tr>';
+          if ($time_point == 'future') {
+            $free_slots += 1;
+          }
+        }
+        else {
+          if (isset($slot[$i])) {
+            $subject = $slot[$i];
+            $schedule .= '<tr>
+            <td width="10%">' . $show_time . '</td>
+            <td width="25%">' . $subject[0] . '</td>
+            <td width="35%">' . $subject[1] .'</td>
+            <td width="20%">' . $subject[2] . '</td>
+            <td width="10%"><a href="index.php?page=edit_subject&exp='. $experiment->id .'&date='. $date .'&time='. $time .'&subject='. $i .'"><img src="images/edit.png" width="16" height="16" style="vertical-align: bottom" /></a>&nbsp;<a href="index.php?page=delete_subject&exp='. $experiment->id .'&date='. $date .'&time='. $time .'&subject='. $i .'"><img src="images/delete.png" width="16" height="16" style="vertical-align: bottom" /></a></td>
+            </tr>';
+            if ($time_point == 'past') {
+              $past_slots += 1;
+            }
+            if ($time_point == 'present') {
+              $present_slots += 1;
+            }
+            if ($time_point == 'future') {
+              $future_slots += 1;
+            }
+          }
+          else {
+            $schedule .= '<tr>
+            <td width="10%">' . $show_time . '</td>
+            <td width="25%">-</td>
+            <td width="35%">-</td>
+            <td width="20%">-</td>
+            <td width="10%"><a href="index.php?page=add_subject&exp='. $experiment->id .'&date='. $date .'&time='. $time .'"><img src="images/add.png" width="16" height="16" style="vertical-align: bottom" /></a></td>
+            </tr>';
+            if ($time_point == 'future') {
+              $free_slots += 1;
+            }
+          }
         }
       }
     }
+    if ($unix_date == $unix_tomorrow) {
+      $schedule .= '<tr><td colspan="2"></td><td colspan="3"><button id="reminders" class="green">Send all reminder emails for tomorrow</button></td></tr>';
+    }
   }
-  if ($unix_date == $unix_tomorrow) {
-    $schedule .= '<tr><td colspan="2"></td><td colspan="3"><button id="reminders" class="green">Send all reminder emails for tomorrow</button></td></tr>';
-  }
+  $schedule .= '</table></div>';
 }
-$schedule .= '</table></div>';
+else {
+  $schedule .= '<p>No calendar has been set up for this experiment.</p>';
+}
 
 if ($experiment->getStatus() == 'closed') {
   $change_status_button = '<button id="change_status" class="orange">Open experiment</button>';
@@ -87,5 +120,37 @@ if ($experiment->getStatus() == 'closed') {
 else {
   $change_status_button = '<button id="change_status" class="red">Close experiment</button>';
 }
+
+$total_slots = $past_slots + $present_slots + $future_slots + $free_slots;
+if ($total_slots != 0) {
+  $percentage_past = ($past_slots / $total_slots) * 100;
+  $percentage_present = ($present_slots / $total_slots) * 100;
+  $percentage_future = ($future_slots / $total_slots) * 100;
+  $percentage_free = ($free_slots / $total_slots) * 100;
+}
+
+$legend = array();
+
+if ($percentage_past != 0) {
+  $past_div = '<div style="background-color: #5D9648; height: 10px; width: ' . $percentage_past . '%; float: left;"></div>';
+  $legend[] = '<span style="color: #5D9648">Completed: ' . $past_slots . '</span>';
+}
+
+if ($percentage_present != 0) {
+  $present_div = '<div style="background-color: #3B6C9D; height: 10px; width: ' . $percentage_present . '%; float: left;"></div>';
+  $legend[] = '<span style="color: #3B6C9D">Today: ' . $present_slots . '</span>';
+}
+
+if ($percentage_future != 0) {
+  $future_div = '<div style="background-color: #E8A13D; height: 10px; width: ' . $percentage_future . '%; float: left;"></div>';
+  $legend[] = '<span style="color: #E8A13D">Upcoming: ' . $future_slots . '</span>';
+}
+
+if ($percentage_free != 0) {
+  $free_div = '<div style="background-color: #EFEFEF; height: 10px; width: ' . $percentage_free . '%; float: left;"></div>';
+  $legend[] = 'Available slots: ' . $free_slots;
+}
+
+$legend = implode('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;', $legend);
 
 ?>
