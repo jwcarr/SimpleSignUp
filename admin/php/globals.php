856f17e40f3de526c37b60c561643f332c51fb41
Jon Carr <jwcarr@users.noreply.github.com>
2014-12-05 19:41:35 +0000

    Updated generateMenu() to use icons

--- admin/php/globals.php
+++ admin/php/globals.php
@@ -16,16 +16,16 @@ date_default_timezone_set('UTC');
 
 function generateMenu($name, $current_page) {
   $menu_items = array('main'=>'My experiments', 'new'=>'New experiment', 'change_details'=>'Change my details', 'change_password'=>'Change my password');
-  $menu = '<p><strong>'. $name .'</strong></p><ul>';
+  $menu = '<p><strong>'. $name .'</strong></p>';
   foreach ($menu_items as $page=>$description) {
     if ($page == $current_page) {
-      $menu .= '<li>'. $description .'</li>';
+      $menu .= '<p><img src="images/'. $page .'_black.png" width="16" height="16" style="vertical-align: bottom" />&nbsp;&nbsp;'. $description .'</p>';
     }
     else {
-      $menu .= '<li><a href="index.php?page='. $page .'">'. $description .'</a></li>';
+      $menu .= '<p><a href="index.php?page='. $page .'" onMouseOver="document.' . $page . '.src=\'images/' . $page . '_black.png\'" onMouseOut="document.' . $page . '.src=\'images/' . $page . '.png\'"><img name="' . $page . '" src="images/' . $page . '.png" width="16" height="16" style="vertical-align: bottom" />&nbsp;&nbsp;'. $description .'</a></p>';
     }
   }
-  $menu .= '<li><a href="index.php?page=logout">Logout</a></li></ul>';
+  $menu .= '<p><a href="index.php?page=logout" onMouseOver="document.logout.src=\'images/logout_black.png\'" onMouseOut="document.logout.src=\'images/logout.png\'"><img name = "logout" src="images/logout.png" width="16" height="16" style="vertical-align: bottom" />&nbsp;&nbsp;Logout</a></p>';
   return $menu;
 }
 
