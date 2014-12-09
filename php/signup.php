8bb6c981a59e18ada64d5083f98abd325189da7b
Jon Carr <jwcarr@users.noreply.github.com>
2014-12-09 14:54:00 +0000

    Only show requirement checkboxes for multiperson experiments

--- php/signup.php
+++ php/signup.php
@@ -9,4 +9,11 @@ if ($experiment->getStatus() != 'open') {
   $page = 'fully_subscribed';
 }
 
+else {
+  $requirement_checkboxes = $experiment->printRequirementCheckboxes();
+  if ($requirement_checkboxes != False) {
+    $requirements = '<h2>Requirements</h2><p>Please indicate that you meet the requirements for this experiment.</p><div id="reqs">'. $requirement_checkboxes .'</div>';
+  }
+}
+
 ?>
