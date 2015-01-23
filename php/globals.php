bdeba39d3fcf3c892955adff935b3074a3c15f27
Jon Carr <jwcarr@users.noreply.github.com>
2015-01-23 10:58:29 +0000

    Added $admin_email_address global parameter
    Set an admin email address to which logs are sent

--- php/globals.php
+++ php/globals.php
@@ -3,6 +3,10 @@
 // Path to data folder from SimpleSignUp root
 $data_path = '../../server_data/ssu/';
 
+
+// Admin email address; logs will be sent here
+$admin_email_address = 'PUT-AN-ADMIN-EMAIL-HERE';
+
 // Set timezone for timestamps (default = UTC)
 date_default_timezone_set('UTC');
 
